aad66514682f550ef1e43e187471317dddd5bbae
serafima8 <147659413+serafima8@users.noreply.github.com>
2023-11-02 21:58:23 +0200

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        //3 вариант
+
+        // Задание 1.Вывести на экран вертикальную строку из 18 символов, отодвинутую от левого края экрана на 5 позиций.
+        //static void Func()
+        //{
+        //    for (int i = 1; i <= 18; i++)
+        //    {
+        //        Console.WriteLine("     #");
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Func();
+        //    Console.ReadKey();
+        //}
+
+
+
+        //Задание 2.
+        //static void Func()
+        //{
+        //    for (int i = 1; i <= 9; i++)
+        //    {
+        //        for (int a = 1; a <= i; a++) 
+        //        {
+        //            Console.Write("#");
+        //        }
+
+        //        Console.WriteLine();
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Func();
+        //    Console.ReadKey();
+        //}
+
+
+
+        //Задание 3. Вычислить значение выражения, определив и использовав дополнительный метод.
+        //public static double Func(double x, double y)
+        //{
+        //    double z = (x + Math.Sin(x)) / (Math.Sin(y) + y);
+        //    return z;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    double func = Func(2, 5) + Func(6, 3) + Func(1, 4);
+        //    Console.Write($"y = {func}");
+        //    Console.ReadKey();
+        //}
+
+
+
+        //Задание 4.Найти s = max(a, b) + max(c, d), используя вспомогательную функцию(метод).
+        //public static int Max(int a, int b)
+        //{
+        //    return (a > b) ? a : b;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Введите число a: ");
+        //    int a = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Введите число b: ");
+        //    int b = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Введите число c: ");
+        //    int c = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Введите число d: ");
+        //    int d = Convert.ToInt32(Console.ReadLine());
+        //    int s = Max(a, b) + Max(c, d);
+        //    Console.Write($"s = {s}");
+        //    Console.ReadKey();
+        //}
+
+    }
+}
